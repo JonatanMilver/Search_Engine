d8bc5b723ebb7015fcc70975173c5815ccb486e1
JonatanMilver <48964784+JonatanMilver@users.noreply.github.com>
2020-11-30 16:13:23 +0200

    fixed uploading issues with the submission system

--- configuration.py
+++ configuration.py
@@ -1,11 +1,19 @@
+import os
+
+
 class ConfigClass:
     def __init__(self, corpus_path, output_path, stemming=False):
-        # self.corpusPath = corpus_path
-        self.corpusPath = "C:\\Users\\yonym\\Desktop\\ThirdYear\\IR\\engineV1\\Data\\date=07-16-2020\\covid19_07-16.snappy.parquet"
+        self.corpusPath = corpus_path
+        try:
+            os.mkdir(output_path)
+        except:
+            pass
+        # self.corpusPath = "C:\\Users\\yonym\\Desktop\\ThirdYear\\IR\\engineV1\\Data\\date=07-16-2020\\covid19_07-16.snappy.parquet"
         self.savedFileMainFolder = output_path
         self.saveFilesWithStem = self.savedFileMainFolder + "/WithStem"
         self.saveFilesWithoutStem = self.savedFileMainFolder + "/WithoutStem"
         self.toStem = stemming
+
 
         print('Project was created successfully..')
 
